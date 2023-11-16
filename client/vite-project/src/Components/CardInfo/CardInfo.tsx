e62e044d57e4ca404fca4bd76ab98f491a754f27
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-11-16 10:19:10 -0500

    completed mobile view beginning on responsive views

--- client/vite-project/src/Components/CardInfo/CardInfo.tsx
+++ client/vite-project/src/Components/CardInfo/CardInfo.tsx
@@ -6,39 +6,43 @@ import Button from '../Button/Button';
 import { ButtonStyle } from '../../enums/component-enums';
 
 
-export default function Card({ cardTitle, icons, button, input, buttonsGroup }: CardComponent) {
-  console.log(buttonsGroup);
+export default function Card({ cardTitle, icons, button, input, buttonsGroup, buttonsGroupTitle }: CardComponent) {
   return (
     <div className='card-container'>
-    <div className='weeb-trade-card'>
-      <h1 className='weeb-trade-text'>{cardTitle}</h1>
-      <div className='weeb-trade-card-inputs'>
-      {Array.isArray(input) ? (
-          <div className='weeb-trade-card-input-one'>
-            {input.map((label, index) => (
-              <div key={index} className='weeb-trade-input-container'>
-                <Input label={label} />
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className='weeb-trade-input-container'>
-            {input && <Input label={input} />}
-          </div>
+      <div className='weeb-trade-card'>
+        <h1 className='weeb-trade-text'>{cardTitle}</h1>
+        <div className='weeb-trade-card-inputs'>
+          {Array.isArray(input) ? (
+            <div className='weeb-trade-card-input-one'>
+              {input.map((label, index) => (
+                <div key={index} className='weeb-trade-input-container'>
+                  <Input label={label} />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className='weeb-trade-input-container'>
+              {input && <Input label={input} />}
+            </div>
           )}
           {buttonsGroup && (
             <div className='weeb-trade-card-additional-buttons'>
-              {buttonsGroup.map((label: string, index: number) => (
-                <Button key={index} label={label} style={ButtonStyle.xSmall} />
-              ))}
+              {buttonsGroupTitle && (
+                <div className='additional-buttons-title'>{buttonsGroupTitle}</div>
+              )}
+              <div className='weeb-trade-card-add-buttons-container'>
+                {buttonsGroup.map((label: string, index: number) => (
+                  <Button key={index} label={label} style={ButtonStyle.xSmall} />
+                ))}
+              </div>
             </div>
           )}
-        <div className='weeb-trade-card-button-container'>
-          {button}
-        </div>
+          <div className='weeb-trade-card-button-container'>
+            {button}
+          </div>
         </div>
         {icons && <LoginIcons />}
-    </div>
+      </div>
     </div>
   )
 }
--- client/vite-project/src/Screens/Search/Search.tsx
+++ client/vite-project/src/Screens/Search/Search.tsx
@@ -5,7 +5,8 @@ import './Search.scss'
 export default function Search() {
   const searchLabel = 'Search'
   const buttonsGroupLabel = ['Anime', 'Year', 'Type'];
+  const buttonsGroupTitle = 'Filter by'
   return ( 
-      <Card button={<Button label={searchLabel} />} input= {searchLabel} buttonsGroup={buttonsGroupLabel} />
+      <Card button={<Button label={searchLabel} />} input= {searchLabel} buttonsGroup={buttonsGroupLabel} buttonsGroupTitle={buttonsGroupTitle} />
   )
 }
