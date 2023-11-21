300b550006192cd37d6b1ef76605ca46dea7d86b
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-11-21 11:34:35 -0500

    removed console logs

--- client/vite-project/vite.config.ts
+++ client/vite-project/vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
 dotenv.config();
-console.log('VITE_REACT_APP_ANON_KEY:', process.env.VITE_REACT_APP_ANON_KEY);
-console.log('VITE_REACT_APP_SUPABASEURL:', process.env.VITE_REACT_APP_SUPABASEURL);
 
 export default defineConfig({
   plugins: [react()],
