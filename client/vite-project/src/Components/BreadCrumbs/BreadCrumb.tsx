eb91e613807c3c1e0dba227fa9fdc9a3a430aba0
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-11-22 13:29:51 -0500

    adding in breadcrumbs

--- client/vite-project/src/Components/BreadCrumbs/BreadCrumb.tsx
+++ client/vite-project/src/Components/BreadCrumbs/BreadCrumb.tsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import BreadCrumbsComponent from '../../Interfaces/component-interface';
 
 export default function BreadCrumbs({ breadCrumbs }: BreadCrumbsComponent) {
+  const { pathname } = useLocation();
+  const paths = pathname.split('/').filter((path) => path !== '');
+
+  const breadcrumbs = paths.map((path, index) => {
+    const breadcrumb = {
+      label: path, // You can format the label as needed
+      path: `/${paths.slice(0, index + 1).join('/')}`,
+    };
+
+    return breadcrumb;
+  });
+
   return (
     <nav>
       <ul>
-        {breadCrumbs.map((breadcrumb:any, index:string) => (
+        {breadcrumbs.map((breadcrumb, index) => (
           <li key={index}>
             {breadcrumb.path ? (
               <Link to={breadcrumb.path}>{breadcrumb.label}</Link>
--- client/vite-project/src/Screens/AnimeDetails/AnimeDetails.tsx
+++ client/vite-project/src/Screens/AnimeDetails/AnimeDetails.tsx
@@ -1,13 +1,20 @@
 import { useParams } from "react-router-dom"
 import './AnimeDetails.scss';
+import BreadCrumbs from "../../Components/BreadCrumbs/BreadCrumb";
 
 export default function AnimeDetails(props) {
 
   const { title } = useParams();
   const anime = props.anime.find((anime) => anime.Title === title);
+  const breadcrumbData = [
+    { label: 'Home', path: '/' },
+    { label: 'Series', path: '/browse/series' },
+    { label: title, path: `/anime/${title}` }
+  ]
   return (
     <>
       <div className='weeb-anime-details'>
+        <BreadCrumbs breadcrumbs={breadcrumbData} />
         <h1 className='weeb-detail-title'>{title}</h1>
         <div className='weeb-detail-img-container'>
           <img className='weeb-detail-img' src={anime.imgdetail} alt={title} />
