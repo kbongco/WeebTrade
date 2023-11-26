312eadea0fd85124b1b029c751c10d6860b0478a
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-11-26 14:19:25 -0500

    completed anime details page

--- client/vite-project/src/Components/ProductCard/ProductCard.tsx
+++ client/vite-project/src/Components/ProductCard/ProductCard.tsx
@@ -2,24 +2,26 @@ import './ProductCard.scss';
 
 export default function ProductCard(props) {
   const { imgLink, FigureName, FigureType, figureType  } = props;
-  console.log(props)
-  console.log(props);
   console.log(FigureType);
+  console.log(figureType)
+  // FigureType represents the ID number that corresponds to the figureTypeId\
 
+  const mapIdToText = (id: any) => {
+    const matchingType = figureType.find((type:any) => type.id === id);
+    return matchingType ? matchingType.FigureType : 'Unknown Type';
+  }
+
+  const figureTypeText = mapIdToText(FigureType);
 
   return (
     <>
       <div className='product-card'>
         <div className='product-card-img-container'>
-          {/* <img className='product-img' src='https://i.ibb.co/1KYn1DJ/Screen-Shot-2023-11-21-at-1-52-26-PM.png'/> */}
           <img className='product-img' src={imgLink}/>
         </div>
         <div className='product-card-text'>
-          {/* <p>Anya and Bond Forger Sleepy Time</p> */}
           <p>{FigureName}</p>
-          {/* <p>PVC Figure</p> */}
-          {/* <p>{FigureType}</p> */}
-          {/* <p>{figureType.map((type:any) => idToText(type.id)).join(', ')}</p> */}
+          <p>{figureTypeText}</p>
           <div className='product-card-details'>
             <a className='product-card-view' href='/'>View details</a>
           </div>
