9418d819fa88a280d703c56ba2fdef418301ec63
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-11-28 14:39:15 -0500

    revamped mobile and desktop views

--- client/vite-project/src/Screens/FigureDetails/FigureDetails.tsx
+++ client/vite-project/src/Screens/FigureDetails/FigureDetails.tsx
@@ -11,31 +11,34 @@ export default function FigureDetails(props) {
   return (
     <>
       <div className='figure-details-container'>
-        <div className='figure-details-name'>
-          <h1 className='figure-details-title'>Loid Forger Party Ver</h1>
+        <div className='figure-photo-container'>
+          <img className='figure-photo' src='https://i.ibb.co/hZtfZKY/Screen-Shot-2023-11-21-at-1-53-11-PM.png' />
+        </div>
+        <div className='figure-info-container'>
+                  <div className='figure-details-name'>
+          <h1>Loid Forger Party Ver</h1>
+        </div>
+          <div className='figure-photo-value-container'>
+            <div className='figure-star-container'>
+              <Ratings />
+            </div>
+            <p className='figure-value-text'>$$</p>
           </div>
-          <div className='figure-photo-container'>
-            <img className='figure-photo' src='https://i.ibb.co/hZtfZKY/Screen-Shot-2023-11-21-at-1-53-11-PM.png' />
-        </div>
-        <div className='figure-photo-value-container'>
-          <div className='figure-star-container'>
-            <Ratings />
+          <div className='figure-description-container'>
+            <p className='description-info'>Description:</p>
+            <p>Size: 7.8in</p>
+            <p>Material: PVC</p>
+            <p>Manufacturer: Sega</p>
           </div>
-          <p className='figure-value-text'>$$</p>
-        </div>
-        <div className='figure-description-container'>
-          <p>Description:</p>
-          <p>Size: 7.8in</p>
-          <p>Material: PVC</p>
-          <p>Manufacturer: Sega</p>
-        </div>
-        <div className='figure-accordion-conatiner'>
-          <Accordion title={title} content={content} />
-        </div>
-        <div className='figure-button-container'>
-          <Button label={'Make an Offer'} />
+          <div className='figure-accordion-conatiner'>
+            <Accordion title={title} content={content} />
+          </div>
+          <div className='figure-button-container'>
+            <Button label={'Make an Offer'} />
+          </div>
         </div>
       </div>
+
     </>
   )
 }
