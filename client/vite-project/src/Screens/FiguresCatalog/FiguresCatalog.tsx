0d1bd52719359698e3680edd52178970aa590ebb
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-12-28 17:52:54 -0500

    adding search functionality

--- client/vite-project/src/Screens/FiguresCatalog/FiguresCatalog.tsx
+++ client/vite-project/src/Screens/FiguresCatalog/FiguresCatalog.tsx
@@ -7,20 +7,23 @@ import { Figures } from "../../Interfaces/anime-interface";
 import getFigureTypesForFigures from "../../utils/getTypesForFigures";
 import './FiguresCatalog.scss';
 import Pagination from "../../Components/Pagination/Pagination";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function FiguresCatalog(props) { 
+export default function FiguresCatalog(props) {
   const figures: any = useFigures();
   const figureTypes = props.figureTypes;
   const allFigureTypes = getFigureTypesForFigures(figures.figures, figureTypes);
   const [searchItem, setSearchItem] = useState('');
+  const [searchedFigures, setSearchedFigures] = useState<any>([]);
   const totalItems = figures.figures.length;
-  console.log(figures.figures.length)
+  const allFigures = figures.figures;
   const itemsPerPage = 6;
   const [currentPage, setCurrentPage] = useState(1);
   const startIndex = (currentPage - 1) * itemsPerPage;
-  console.log(startIndex);
   const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
-  const setOfFigures = figures.figures.slice(startIndex, endIndex);
+  // const setOfFigures = figures.figures.slice(startIndex, endIndex);
+  const setOfFigures = searchItem.length > 0 ? searchedFigures : allFigures.slice(startIndex, endIndex);
 
 
 
@@ -29,27 +32,52 @@ export default function FiguresCatalog(props) {
   }
 
   const handleSearch = () => {
-    console.log('searching:', searchItem)
-  }
+    console.log('searching:', searchItem);
+  
+    if (searchItem.length > 0) {
+      const matchingFigures = allFigures.filter((figure) =>
+        figure.FigureName.includes(searchItem)
+      );
+  
+      console.log('Matching Figures:', matchingFigures);
+      setSearchedFigures(matchingFigures);
+      setCurrentPage(1);
+    }
+  };
 
 
   const breadcrumbData = [
     { label: 'Home', path: '/' },
-    {label: 'Figures', path:'/browse/figures'}
+    { label: 'Figures', path: '/browse/figures' }
   ]
   return (
     <>
       <BreadCrumbs breadCrumbs={breadcrumbData} />
       <div className='browse-figures-container'>
-        <h1 className='browse-figures-text'>Browse Figures</h1>
-        <div className='browse-figures-search-input'>
-          <Input onChange={handleSearchInputChange} value={searchItem} />
-          <button onClick={handleSearch}>Test</button>
+        <h1 className='browse-figures-text'>Browse By Figures</h1>
+        <div className='browse-figures-search-containers'>
+          <div className='browse-figures-search-input'>
+            <Input onChange={handleSearchInputChange} value={searchItem} />
+          </div>
+          <div className='browse-figures-search-figures'>
+            <button className='browse-figures-search-button ' onClick={handleSearch}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
         </div>
         <div className='browse-figures-card-container'>
-          {setOfFigures.map((figure: Figures, id: number) => (
-            <ProductCard key={id} {...figure} figureType={allFigureTypes}/> 
+          {/* {setOfFigures.map((figure: Figures, id: number) => (
+            <ProductCard key={id} {...figure} figureType={allFigureTypes} />
           ))}
+          {searchItem.length > 0 && searchedFigures.length === 0 && (
+            <p>No figures found</p>
+          )} */}
+                {setOfFigures.map((figure: Figures, id: number) => (
+        <ProductCard key={id} {...figure} figureType={allFigureTypes} />
+      ))}
+      {searchItem.length > 0 && searchedFigures.length === 0 && (
+        <p>No figures found</p>
+      )}
         </div>
         <div className='browse-figures-pagination-container'>
           <Pagination
@@ -60,5 +88,5 @@ export default function FiguresCatalog(props) {
             lastPage={Math.ceil(totalItems / itemsPerPage)} />
         </div>
       </div>
-  </>)
+    </>)
 }
